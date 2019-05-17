0f5839e95f648bf1d2ace6341477083650528ffc
Aniruddh Iyengar <aniiyengar@berkeley.edu>
2019-05-16 19:00:39 -0700

    fasdfa

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 from nba import NBAScore
 import twitter
 import os
+import json
 
 if __name__ == '__main__':
+
+	with open('api_keys.json') as f:
+		keys = json.loads(f.read())
+
 	api = twitter.Api(
-		consumer_key=os.environ['TW_API_KEY'],
-		consumer_secret=os.environ['TW_API_SECRET'],
-		access_token_key=os.environ['TW_ACCESS_KEY'],
-		access_token_secret=os.environ['TW_ACCESS_SECRET'])
+		consumer_key=keys['consumer_key'],
+		consumer_secret=keys['consumer_secret'],
+		access_token_key=keys['access_key'],
+		access_token_secret=keys['access_secret'])
 
 	api.PostUpdate(NBAScore().get_status())
